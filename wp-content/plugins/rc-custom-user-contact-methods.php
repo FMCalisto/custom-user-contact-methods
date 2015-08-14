a32d0ea635f97785019eb7b51aaf57f1e7cad807
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-08-15 00:50:20 +0100

    Define Custom Fields
    Defining the custom fields.

--- wp-content/plugins/rc-custom-user-contact-methods.php
+++ wp-content/plugins/rc-custom-user-contact-methods.php
@@ -8,3 +8,13 @@ Author: Remi Corson
 Author URI: http://remicorson.com
 Contributors: corsonr
 */
+
+$extra_fields =  array(
+  array( 'facebook', __('Facebook Username', 'rc_cucm'), true ),
+  array( 'twitter', __('Twitter Username', 'rc_cucm'), true ),
+  array( 'googleplus', __('Google+ ID', 'rc_cucm'), true ),
+  array( 'linkedin', __('Linked In ID', 'rc_cucm'), false ),
+  array( 'pinterest', __('Pinterest Username', 'rc_cucm'), false ),
+  array( 'wordpress', __('WordPress.org Username', 'rc_cucm'), false ),
+  array( 'phone', __('Phone Number', 'rc_cucm'), true )
+);
