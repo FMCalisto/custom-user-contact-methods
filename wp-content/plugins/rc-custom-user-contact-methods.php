989b6aa75f2815556b59df19087def94b5a4a306
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-08-15 01:01:34 +0100

    Update rc-custom-user-contact-methods.php

--- wp-content/plugins/rc-custom-user-contact-methods.php
+++ wp-content/plugins/rc-custom-user-contact-methods.php
@@ -1,11 +1,10 @@
 <?php
 /*
 Plugin Name: Custom User Contact Methods
-Plugin URL: http://remicorson.com/
 Description: Add custom fields to users "contact" section
 Version: 1.0
-Author: Remi Corson
-Author URI: http://remicorson.com
+Author: Francisco Maria Calisto
+Author URI: http://franciscocalisto.me/
 Contributors: corsonr
 */
 
